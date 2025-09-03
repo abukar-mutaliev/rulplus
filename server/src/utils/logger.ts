bc3208849ecd9c524db8ce7f17def9c17fb90977
Abu <abubakr.ingush@gmail.com>
2025-09-03 17:58:10 +0300

    fix error build

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,5 +1,5 @@
 import * as winston from 'winston';
-import * as DailyRotateFile from 'winston-daily-rotate-file';
+import DailyRotateFile = require('winston-daily-rotate-file');
 import { config } from '../config/index.js';
 
 // Формат логов
